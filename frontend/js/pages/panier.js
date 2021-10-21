1679d1169531a3efdd2d7c9f0296448933449231
Antoine <antoinejonville@yahoo.fr>
2021-10-21 16:24:24 +0200

    affichage du LS dans le DOM ok

--- frontend/js/pages/panier.js
+++ frontend/js/pages/panier.js
@@ -1,43 +1,39 @@
 class Panier extends Page {
     constructor(domTarget) {
-        super(domTarget, null);
+        super(domTarget)
+        this.DOM = document.createElement('section');
+        this.DOM.classList.add('section_product')
+        domTarget.appendChild(this.DOM);
         this.render();
-    };
+    }
 
 
-     /**
- * génère la fiche produit
- * 
- * @param {FicheProduit} props    [props description]
- * 
- * @return {String}          le contenu html de la page
- */
-    render(props) {
+    render() {
         // Variable qui récupère le contenu du LS
-        let allProducts = JSON.parse(localStorage.getItem("products"));
+        const allProducts = JSON.parse(localStorage.getItem("products"));
         //console.log(allProducts); // Vérifie qu'on a bien les produits enregistré dans le LS
         
         // Si le panier est vide, afficher le message
         if (allProducts === null) {
             //console.log("je suis vide !"); // Vérifie que ça fonctionne
             this.DOM.innerHTML = `
-                <p class="empty_basket">Votre panier est vide, <a class="return_home" onclick="history.go(-1);">Veuillez retourner à nos produits</a></p>
-                `
+            <p class="empty_basket">Votre panier est vide, <a class="return_home" onclick="history.go(-1);">Veuillez retourner à nos produits</a></p>
+            `
         } else {  //Sinon afficher les produits enregistré dans le LS
             // console.log("Je ne suis pas vide !");  // Vérifie que ça fonctionne
-            allProducts.forEach((products) => {
-                products.value = allProducts
-                console.log(products.name);
-                this.DOM.innerHTML =
-                    `<h3>${products.name}</h3>
-                    <p>${products.price}</p>
-                    <p>${products.colors}</p>
-                    `
-            })
+            for (let product of allProducts) {
+                // product.value = allProducts
+                console.log(product.name);
+                this.DOM.innerHTML += `
+                <article>
+                    <h2>${product.name}</h2>
+                    <p>${product.colors}</p>
+                    <span>${product.price / 100 + ".00 €"}</span>
+                </article>
                 
                 
+                `
+            }
         }
     }
 }
-    
-                
